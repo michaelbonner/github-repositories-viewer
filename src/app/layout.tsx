0a631bca76ad33c8599385a1c3e9a3f5682e3072
Michael Bonner <michaelwbonner@gmail.com>
2023-09-14 14:47:04 -0600

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,37 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <footer className="text-gray-700 font-light text-center max-w-lg mx-auto">
+          <a
+            className="flex items-center justify-center space-x-2 w-full pt-10"
+            href="https://bootpackdigital.com/?utm_source=github-repository-viewer"
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              alt="Bootpack Digital"
+              loading="lazy"
+              width="300"
+              height="90"
+              src="https://bootpackdigital.com/bpd-color-horizontal.svg"
+            />
+          </a>
+          <p className="block mt-4">
+            Come check out our custom web design, web development, software, and
+            mobile apps at{" "}
+            <a
+              className="text-blue-600 underline"
+              href="https://bootpackdigital.com/?utm_source=github-repository-viewer"
+            >
+              Bootpack Digital
+            </a>
+          </p>
+          <p className="block mt-2">
+            <span className="text-sm">©</span>2023 Bootpack Digital
+          </p>
+        </footer>
+      </body>
     </html>
   );
 }
