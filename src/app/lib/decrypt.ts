90c7f2c25f93ac4ff140ddbf4e36dbfacc11fae5
Michael Bonner <michaelwbonner@gmail.com>
2023-09-14 17:33:45 -0600

    try/catch for decrypt

--- src/app/lib/decrypt.ts
+++ src/app/lib/decrypt.ts
@@ -4,9 +4,13 @@ import enc from "crypto-js/enc-utf8";
 export const decrypt = (encryptedText: string) => {
   if (!encryptedText) return "";
 
-  var bytes = AES.decrypt(
-    encryptedText,
-    process.env.NEXT_PUBLIC_ENCRYPTION_KEY || ""
-  );
-  return JSON.parse(bytes.toString(enc));
+  try {
+    var bytes = AES.decrypt(
+      encryptedText,
+      process.env.NEXT_PUBLIC_ENCRYPTION_KEY || ""
+    );
+    return JSON.parse(bytes.toString(enc));
+  } catch (e) {
+    return "";
+  }
 };
